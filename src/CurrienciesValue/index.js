a6c5015248e3612e8e4b98ed4e4866971d96a3a6
Lukasz Gasior <lukasz.gasior7@gmail.com>
2023-07-05 19:23:01 +0200

    added error handling

--- src/CurrienciesValue/index.js
+++ src/CurrienciesValue/index.js
@@ -4,25 +4,46 @@ import { useEffect, useState } from "react";
 
 export const useRatesData = () => {
     const [ratesData, setRatesData] = useState(({
-        state: "Wczytywanie...",
+        state: "loading",
     }));
 
     useEffect(() => {
 
-        axios.get("https://api.exchangerate.host/latest?base=PLN&symbols=USD,EUR,CHF,GBP,CNY,JPY")
-            .then(response => {
-                const { rates, date } = response.data;
+const fetchRates = () => (async() => {
+    try {
+        const response = await axios ("https://api.exchangerate.host/latest?base=PLN&symbols=USD,EUR,CHF,GBP,CNY,JPY");
+        const {rates, date } = response.data;
+        setRatesData({
+            state: "ok",
+            rates,
+            date,
+        });
+    } catch(error) {
+        console.error("Coś poszło nie tak");
+        setRatesData(({
+            state: "error",
+        }))
 
-                setRatesData({
-                    state: "Załadowano",
-                    rates,
-                    date,
-                });
 
-            });
 
+    }
+
+}) ();
+setTimeout(fetchRates, 2000);
     }, []);
 
     return ratesData;
 
 }
+
+/*        axios.get("https://api.exchangerate.host/latest?base=PLN&symbols=USD,EUR,CHF,GBP,CNY,JPY")
+.then(response => {
+    const { rates, date } = response.data;
+
+    setRatesData({
+        state: "Załadowano",
+        rates,
+        date,
+    });
+
+}); */
